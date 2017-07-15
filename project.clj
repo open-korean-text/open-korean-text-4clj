f3b59e1656249705c515a21f7ed9603bd3d36ada
Seonho Kim <ksseono@gmail.com>
2017-07-15 19:29:19 +0900

    update profiles for midje

--- project.clj
+++ project.clj
@@ -3,8 +3,13 @@
   :url "http://github.com/open-korean-text/open-korean-text-4clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.openkoreantext/open-korean-text "2.1.0"]
                  [midje "1.8.3"]]
+
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}}
+  
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]])
