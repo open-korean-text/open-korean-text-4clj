7e51b9ac573ccefea6acdfa9bec222b77f411dae
Seonho Kim <ksseono@gmail.com>
2018-04-12 22:36:04 +0900

    update dependency versions

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.openkoreantext/open-korean-text "2.1.2"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.openkoreantext/open-korean-text "2.2.0"]]
 
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}}
 
   :repl-options {:init-ns open-korean-text-4clj.core}
