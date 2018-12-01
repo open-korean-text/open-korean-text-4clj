8b1b4c4f9a385a9f4e669a96fd7d5d2cccc8faff
Seonho Kim <ksseono@gmail.com>
2018-12-01 17:42:24 +0900

    change midje to clojure.test

--- project.clj
+++ project.clj
@@ -7,9 +7,6 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.openkoreantext/open-korean-text "2.3.0"]]
 
-  :profiles {:dev {:dependencies [[midje "1.9.1"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
-
   :repl-options {:init-ns open-korean-text-4clj.core}
 
   :lein-release {:deploy-via :clojars})
